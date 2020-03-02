5d49c65c926d56844f0f45a45fb5df0bcfc7e936
1374240204@qq.com <1374240204@qq.com>
2020-03-02 21:46:25 +0800

    day01_replay

--- day01/11test_thread.py
+++ day01/11test_thread.py
@@ -8,26 +8,21 @@ from multiprocessing import Process
 # 如果,你烧水,
 # 打水,插电, 等十分钟,你就是干等 啥都不干, 就是单线程
 # # 打水插电, 等的过程中, 还在刷微博, 你就是多线程...
-# import time
-#
-# def boil_water():
-#     print('烧水, 等待十秒钟')
-#     time.sleep(20)
-#     print('开了')
-#
-#
-# def word(number):
-#     print('记单词: ', number, '个')
-#
-#
-# if __name__ == '__main__':
-#     boil_water_task = Thread(target=boil_water)
-#     word_task = Thread(target=word, args=(100, ))
-#     boil_water_task.start()
-#     word_task.start()
+import time
 
-# i = 0
-for i in  range(1,5):
-    print(i)
+def boil_water():
+    print('烧水, 等待十秒钟')
+    time.sleep(20)
+    print('开了')
 
 
+def word(number):
+    print('记单词: ', number, '个')
+    return None
+
+
+if __name__ == '__main__':
+    boil_water_task = Thread(target=boil_water)
+    word_task = Thread(target=word, args=(100, ))
+    boil_water_task.start()
+    word_task.start()
